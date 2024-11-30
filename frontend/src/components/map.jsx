b2b40049a362fedb8d5faf2cf8e867dc8f136c48
faizan <faizantayyab37@gmail.com>
2024-11-30 14:42:54 +0500

    updated ui

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,69 +1,44 @@
 import { useEffect, useState } from "react";
 
 const Map = (data) => {
-    const {mapData}= data
-    const [iframeWidth, setIframeWidth] = useState(600);  // Default width
-    const [iframeHeight, setIframeHeight] = useState(450); // Default height
-  
-    // Update iframe dimensions based on the window size
-    useEffect(() => {
-      const handleResize = () => {
-        // Calculate the new width and height dynamically
-        const newWidth = window.innerWidth * 0.8;  // 80% of window width
-        const newHeight = window.innerHeight * 0.6; // 60% of window height
-  
-        setIframeWidth(newWidth);
-        setIframeHeight(newHeight);
-      };
-  
-      // Listen for window resize events
-      window.addEventListener('resize', handleResize);
-  
-      // Initial call to set iframe dimensions
-      handleResize();
-  
-      // Cleanup the event listener on component unmount
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-  
-    // Dynamically set the iframe HTML with updated width and height
-    const updatedMapData = mapData.replace(
-      /width="\d+"/,
-      `width="${iframeWidth}"`
-    ).replace(
-      /height="\d+"/,
-      `height="${iframeHeight}"`
-    );
-  
-    return <div className="google-map-code flex justify-center" dangerouslySetInnerHTML={{ __html: updatedMapData }} />;
+  const { mapData } = data;
+  const [iframeWidth, setIframeWidth] = useState(600); // Default width
+  const [iframeHeight, setIframeHeight] = useState(450); // Default height
+  // Update iframe dimensions based on the window size
+  useEffect(() => {
+    const handleResize = () => {
+      // Calculate the new width and height dynamically
+      const newWidth = window.innerWidth * 0.8; // 80% of window width
+      const newHeight = window.innerHeight * 0.4; // 60% of window height
 
+      setIframeWidth(newWidth);
+      setIframeHeight(newHeight);
+    };
+
+    // Listen for window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Initial call to set iframe dimensions
+    handleResize();
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, [data]);
+
+  // Dynamically set the iframe HTML with updated width and height
+  const updatedMapData = mapData
+    .replace(/width="\d+"/, `width="${iframeWidth}"`)
+    .replace(/height="\d+"/, `height="${iframeHeight}"`);
+
+  return (
+    <div
+      className="google-map-code flex justify-center"
+      dangerouslySetInnerHTML={{ __html: updatedMapData }}
+    />
+  );
 };
 
 export default Map;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // // import { useState } from "react";
 
